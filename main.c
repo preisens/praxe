e4ace509293552ad557618f72f14c33ea33ee888
preisens <p.svehelka.st@spseiostrava.cz>
2023-06-01 23:27:07 +0200

    V3.1
    Final version

--- main.c
+++ main.c
@@ -6,10 +6,6 @@ void main()
     SetWindowLong(console, GWL_STYLE, GetWindowLong(console, GWL_STYLE) & ~WS_MAXIMIZEBOX & ~WS_SIZEBOX);
     ShowWindow(console, SW_MAXIMIZE);
     srand(time(NULL));
-   
-    Generate_map();
-
-    StartPlayer();
 
     Welcome();
 
